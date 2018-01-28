2f8b924579dd5a8b48fd6250c4528fc8aeb7d194
Vladimir Perov <vperov@acumatica.com>
2018-01-28 09:55:50 -0600

    build script compatible with nuget

--- Build/build.bat
+++ Build/build.bat
@@ -1,9 +1,9 @@
-rmdir "%CD%\..\Packages\Temp" /s /q > NUL 2>&1
-rmdir "%CD%\..\Packages\" /s /q > NUL 2>&1
-mkdir "%CD%\..\Packages\Temp" > NUL 2>&1
+rmdir "%CD%\..\CstPackages\Temp" /s /q > NUL 2>&1
+rmdir "%CD%\..\CstPackages\" /s /q > NUL 2>&1
+mkdir "%CD%\..\CstPackages\Temp" > NUL 2>&1
 
-XCOPY %CD%\..\TeamZetaUI "%CD%\..\Packages\Temp" /S /E /H /R /Y > NUL
+XCOPY %CD%\..\TeamZetaUI "%CD%\..\CstPackages\Temp" /S /E /H /R /Y > NUL
 
-PX.CstBuildProject.exe /website "%CD%\..\..\Site" /in "%CD%\..\Packages\Temp" /out "%CD%\..\Packages\TeamZeta.zip" ^
+PX.CstBuildProject.exe /website "%CD%\..\..\Site" /in "%CD%\..\CstPackages\Temp" /out "%CD%\..\CstPackages\TeamZeta.zip" ^
 /include "%CD%\..\TeamZeta\bin\Debug\TeamZeta.dll" "bin\TeamZeta.dll" ^
 /description "Team Zeta 2018"

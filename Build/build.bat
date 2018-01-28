c546927a80ddf2b0f39a076243900c66b5086a12
John Edwards <johnedwards@JOHNEDWARDS9FA4>
2018-01-28 11:00:12 -0600

    ccc

--- Build/build.bat
+++ Build/build.bat
@@ -4,6 +4,6 @@ mkdir "%CD%\..\Packages\Temp" > NUL 2>&1
 
 XCOPY %CD%\..\TeamZetaUI "%CD%\..\Packages\Temp" /S /E /H /R /Y > NUL
 
-PX.CstBuildProject.exe /website "%CD%\..\..\Site" /in "%CD%\..\Packages\Temp" /out "%CD%\..\Packages\TeamZeta.zip" ^
+PX.CstBuildProject.exe /website "C:\Acumatica\17.204.0019\Hackathon2018" /in "%CD%\..\Packages\Temp" /out "%CD%\..\Packages\TeamZeta.zip" ^
 /include "%CD%\..\TeamZeta\bin\Debug\TeamZeta.dll" "bin\TeamZeta.dll" ^
 /description "Team Zeta 2018"
